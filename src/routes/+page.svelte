057370d0fae58d9a1c208c737dda45fdd55d538f
Laurie <laurie.in.japan@gmail.com>
2024-10-10 23:22:45 +0900

    add static adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Laurie's Projects</h1>
+<p>Projects, articles information related to what I do.</p>
