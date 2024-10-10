20970ade5f4935e70051f3bd8046ea06ef2286b8
Laurie <laurie.in.japan@gmail.com>
2024-10-10 23:43:17 +0900

    simple menu bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">Projects</a></li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
+
+<main>
+	<slot></slot>
+</main>
+
+<style>
+	nav ul {
+		list-style: none;
+		display: flex;
+		gap: 1rem;
+	}
+</style>
