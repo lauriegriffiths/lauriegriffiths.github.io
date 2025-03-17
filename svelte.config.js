a97c93592201e001ce1e24cd82ac4474e5e33be7
Laurie <laurie.in.japan@gmail.com>
2025-03-17 15:50:53 +0900

    remove base config for urls

--- svelte.config.js
+++ svelte.config.js
@@ -32,8 +32,8 @@ const config = {
 			fallback: '404.html',
 			precompress: false,
 			strict: true
-		}),
-		paths: { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+		})
+		// paths: { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 	}
 };
 
